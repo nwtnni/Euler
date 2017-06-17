b6a16fb5396fa23c126d8aaba8a3c12377e9f4eb
Newton Ni <cn279@cornell.edu>
2017-06-17 00:29:23 -0400

    Refactoring problem 7

--- 007.py
+++ 007.py
@@ -4,14 +4,17 @@
 Newton Ni
 '''
 
-from sys import argv
 from primes_util import probable_prime
 
-n, i = int(argv[1]) - 1, 1
+LIMIT = 10001
 
-while (n > 0):
-    i += 2
-    if (probable_prime(i)):
-        n -= 1
-print i
+if __name__ == "__main__":
+        
+    n = 2
+    i = 3
 
+    while (n < LIMIT):
+        i += 2
+        n += 1 if probable_prime(i) else 0
+
+    print i
