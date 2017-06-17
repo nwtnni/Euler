a2b780bf5a4efd4e093e520ad1fc101c42942b68
Newton Ni <cn279@cornell.edu>
2017-06-17 00:40:38 -0400

    Refactoring problem 008

--- 008.py
+++ 008.py
@@ -4,28 +4,25 @@
 Newton Ni
 '''
 
-from sys import argv
+LIMIT = 13
 
-digits, max = [], 0
-with open(argv[1], "r") as f:
-    for line in f:
-        for digit in line:
+def times(a, b): return a*b
 
-            if digit.isdigit():
+if __name__ == "__main__":
+    
+    digits = []
+    max = 0
+    with open('008.txt', "r") as f:
+        for line in f:
+            for digit in line.strip():
+
                 digit = int(digit)
-            else:
-                break
 
-            if (len(digits) == int(argv[2])):
-
-                digits.pop(0)
-                digits.append(digit)
-                x = 1
-                for i in range(len(digits)):
-                    x *= digits[i]
-                if (x > max):
-                    max = x
-            else:
-                digits.append(digit)
-
-print max
+                if (len(digits) != LIMIT):
+                    digits.append(digit)
+                else:
+                    digits.pop(0)
+                    digits.append(digit)
+                    product = reduce(times, digits)
+                    max = product if product > max else max
+    print max
