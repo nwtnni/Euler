ad98b257f1b004a26e5eebe1965383b8cc6d7f14
Newton Ni <cn279@cornell.edu>
2017-06-17 00:15:04 -0400

    Refactoring problem 003

--- 003.py
+++ 003.py
@@ -8,9 +8,11 @@ import sys
 from math import sqrt
 from primes_util import probable_prime
 
-n = int(sys.argv[1])
-for i in range(int(sqrt(n)), 0, -1):
-    if (n % i == 0):
-        if (probable_prime(i, 10)):
+LIMIT = 600851475143
+
+if __name__ == "__main__":
+    
+    for i in range(int(sqrt(LIMIT)), 0, -1):
+        if (LIMIT % i == 0) and probable_prime(i, 10):
             print i
             break
