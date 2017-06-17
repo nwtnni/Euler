c47b1a055d5a31a17018580b208b5056c2f11d30
Newton Ni <cn279@cornell.edu>
2017-06-17 00:18:45 -0400

    Refactoring problem 004

--- 004.py
+++ 004.py
@@ -4,10 +4,12 @@
 Newton Ni
 '''
 
-max = 0
-for i in range(100, 1000):
-    for j in range(100, 1000):
-        val = str(i * j)
-        if (max < i * j and val == val[::-1]):
-            max = i * j
-print max
+if __name__ == "__main__":
+    
+    max = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            val = str(i * j)
+            if (max < i * j and val == val[::-1]):
+                max = i * j
+    print max
